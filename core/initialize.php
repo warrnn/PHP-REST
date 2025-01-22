f62bb8181dce6752910f1e0f44e43fa54ff33657
Bryan Mogens Warren <bryanwarren86@gmail.com>
2025-01-22 12:58:10 +0700

    add core functionality for post management and configuration

--- core/initialize.php
+++ core/initialize.php
@@ -2,5 +2,14 @@
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'wamp64' . DS . 'www' . DS . 'PHP-REST');
+// wamp64/www/PHP-REST/includes
+defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT . DS . 'includes');
+defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT . DS . 'core');
+
+// load the config file first
+require_once(INC_PATH . DS . "config.php");
+
+// core classes
+require_once(CORE_PATH . DS . "post.php");
 
 ?>
---
+++ core/post.php
@@ -0,0 +1,49 @@
+<?php
+
+class Post
+{
+    //db stuff
+    private $conn;
+    private $table = 'posts';
+
+    // post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+
+    // constructor with db connection
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // getting posts from our database
+    public function read()
+    {
+        // create query
+        $query = `SELECT 
+        c.name as category_name,
+        p.id,
+        p.category_id,
+        p.title,
+        p.body,
+        p.author,
+        p.creaated_at
+        FROM $this->table p
+        LEFT JOIN category AS c ON p.category_id = c.id
+        ORDER BY p.created_at DESC`;
+
+        // prepare statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
+
+?>
